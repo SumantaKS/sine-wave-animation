8b80dbd47c91f5dc2176b2366ad2ba1d67c63c4f
Sumanta Singh <2758836s@student.gla.ac.uk>
2024-01-03 20:12:27 +0000

    added sine functionality

--- script.js
+++ script.js
@@ -10,19 +10,25 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.enemyImg = new Image();
-    this.enemyImg.src = "img/enemy1.png";
-    this.enemyWidth = 293;
-    this.enemyHeight = 155;
+    this.enemyImg.src = "img/enemy2.png";
+    this.enemyWidth = 266;
+    this.enemyHeight = 188;
     this.width = this.enemyWidth / 2.5;
     this.height = this.enemyHeight / 2.5;
     this.x = Math.random() * (canvasWidth - this.width);
     this.y = Math.random() * (canvasHeight - this.height);
+    this.speed = Math.random() * 4 + 1;
     this.enemyFrame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1); //this ensures each enemy has different flap speed
+    //for sine wave like patter of enemies
+    this.angle = Math.random() * 2; //this determines starting point along the sine wave that the sprite first appears.
+    this.angleSpeed = Math.random() * 0.2; //randomized value is better since it gives each enemy an unique wave/speed/animation
   }
   resetFrame() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    this.x -= this.speed; //for right to left movement
+    this.y += Math.sin(this.angle); //for sine wave
+    this.angle += this.angleSpeed; //every iteration anngle changes. Play with this value to adjust the wave
+    if (this.x + this.width < 0) this.x = canvasWidth; //infinite right to left movement
     if (gameFrame % this.flapSpeed === 0) {
       this.enemyFrame >= 5 ? (this.enemyFrame = 0) : this.enemyFrame++;
     }
@@ -42,7 +48,7 @@ class Enemy {
   }
 }
 
-const enemyCount = 100;
+const enemyCount = 20;
 const enemies = [];
 for (let i = 0; i < enemyCount; i++) {
   enemies.push(new Enemy());
